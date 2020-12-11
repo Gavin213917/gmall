593902394f600aedf3db1f25a9acc28edab7b413
gaoming <gaoming01@wondersgroup.com>
2020-12-11 21:28:15 +0800

    添加打包文件

--- gmall-api/src/main/java/com/liskov/gmall/bean/UserInfo.java
+++ gmall-api/src/main/java/com/liskov/gmall/bean/UserInfo.java
@@ -8,6 +8,9 @@ import java.io.Serializable;
 
 
 public class UserInfo implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
     @Id
     @Column
     @GeneratedValue(strategy = GenerationType.IDENTITY)
