7dd5c1bb678281ffbd90cf31b5f5bf9329dde750
liskov <1462705240@qq.com>
2020-12-12 20:16:16 +0800

    解决服务启动报错没有提供者的问题

--- gmall-user/src/main/java/com/liskov/gmall/user/GmallUserApplication.java
+++ gmall-user/src/main/java/com/liskov/gmall/user/GmallUserApplication.java
@@ -1,10 +1,12 @@
 package com.liskov.gmall.user;
 
+import com.alibaba.dubbo.config.spring.context.annotation.EnableDubbo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import tk.mybatis.spring.annotation.MapperScan;
 
 @SpringBootApplication
+@EnableDubbo
 @MapperScan(basePackages = "com.liskov.gmall.user.mapper")
 public class GmallUserApplication {
 
